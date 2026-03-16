2a42f138ee49b866de86d4d497f989c3de2cee8e
Portalguy598 <ryandmilner@gmail.com>
2026-03-15 17:26:43 -0700

    Added more elements and some styling to Login page, added password visibility toggle

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,48 @@
 import './Login.css'
+import { useState } from 'react';
 
 function Login(){
+    // the below code is used to toggle between visible and hidden password states
+    const [passwdVisibility, setPasswdVisibility] = useState('password');
+    // this function gets placed into the onClick JSX handler
+    const togglePassword = () => {
+        if(passwdVisibility === 'password'){
+            setPasswdVisibility('text');
+        }
+        else {
+            setPasswdVisibility('password');
+        }
+    };
+
     return (
         <div className="container">
-            <div className="header">
-                <h1 className="text">Sign In or Create Account</h1>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-                <div className="input">
-                    <p>Username</p>
-                    <input type="text" />
+            <h1>Aim Rivals</h1>
+            <div className="login-container">
+                <div className="header">
+                    <h2 className="text">Log In or Create Account</h2>
+                    <div className="underline"></div>
                 </div>
-            </div>
-            <div className="inputs">
-                <div className="input">
-                    <p>Password</p>
-                    <input type="password" />
+                <div className="inputs">
+                    <div className="input">
+                        <p className="input-text">Username</p>
+                        <input id="username" type="text" />
+                    </div>
+                    <div className="input">
+                        <p className="input-text">Password</p>
+                        {/* passwdVisibility determines whether it is considered text or password input*/}
+                        <input id="password" type={passwdVisibility} />
+                        {/* onClick determines the function performed when the button is clicked in JSX/TSX */}
+                        <button id="passwd-toggle" onClick={togglePassword}>Toggle</button>
+                    </div>
+                </div>
+                <div className="submit-container">
+                    <p></p>
+                    <button id="signup-submit-button">Create Account</button>
+                    <button id="login_submit-button">Log In</button>
                 </div>
             </div>
         </div>
+        
     )
 }
 

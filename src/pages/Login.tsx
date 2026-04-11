91fc4abcee59ada9be6d081be09413dda2fc3178
Portalguy598 <ryandmilner@gmail.com>
2026-04-11 11:44:37 -0700

    Added ability to navigate to mode page, replaced enums with object due to errors

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,14 @@
 import './Login.css'
 import { useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { auth, db } from '../firebase';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
 
 function Login(){
+    // for navigation
+    const navigate = useNavigate();
+
     // for accessing text info
     const userRef = useRef<HTMLInputElement>(null);
     const emailRef = useRef<HTMLInputElement>(null);
@@ -26,8 +30,14 @@ function Login(){
     // create account handler
     const handleAccountCreation = async (username: string, email: string, password: string) => {
         try{
+            if(username.length < 4){
+                alert('Please include a username with a minimum of 4 characters');
+                return;
+            }
+
             const userCredential = await createUserWithEmailAndPassword(auth, email, password);
             const user = userCredential.user;
+
 
             await setDoc(doc(db, 'users', user.uid), {
                 username: username,
@@ -37,13 +47,17 @@ function Login(){
                 reaction_score: 0,
                 trace_score: 0
             });
+            alert('Account created');
         } catch (e : unknown){
             if(e instanceof Error){
                 console.error('Error Signing Up:', e.message);
+                alert('Could not create account: ' + e.message);
             }
             else{
-                console.error('Unknown Error Occurred When Signing Up')
+                console.error('Unknown Error Occurred When Signing Up');
+                alert('Could not create account');
             }
+            
         }
     };
 
@@ -51,7 +65,7 @@ function Login(){
     const handleLogin = async (email: string, password: string) => {
         try {
             await signInWithEmailAndPassword(auth, email, password);
-            alert('You logged in, yay')
+            navigate('/mode');
         } catch (e : unknown){
             if(e instanceof Error){
                 console.error('Error Logging In:', e.message);
@@ -59,6 +73,7 @@ function Login(){
             else{
                 console.error('Unknown Error Occurred When Logging In')
             }
+            alert('Could not log in');
         }
     };
 
@@ -90,7 +105,7 @@ function Login(){
                 <div className="submit-container">
                     <p></p>
                     <button id="signup-submit-button" onClick={() => {
-                        if(userRef.current !== null && emailRef.current !== null && passwordRef.current != null){
+                        if(userRef.current !== null && emailRef.current !== null && passwordRef.current !== null){
                             handleAccountCreation(userRef.current.value, emailRef.current.value, passwordRef.current.value);
                         }
                         else {
@@ -100,7 +115,7 @@ function Login(){
                     }}>Create Account</button>
                     
                     <button id="login_submit-button" onClick={() => {
-                        if(emailRef.current !== null && passwordRef.current != null){
+                        if(emailRef.current !== null && passwordRef.current !== null){
                             handleLogin(emailRef.current.value, passwordRef.current.value);
                         }
                         else {

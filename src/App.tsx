b10fc2380cdc87188d6d3a7e14df5dbe168b7810
Portalguy598 <ryandmilner@gmail.com>
2026-04-26 18:29:53 -0700

    Made it so /login is default page, cleaned up App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 //import { useState } from 'react'
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, Navigate } from 'react-router-dom';
 import { AuthProvider } from './AuthContext.tsx';
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
@@ -18,34 +18,8 @@ import TracingLeaderboard from './pages/TracingLeaderboard.tsx';
 
 
 function App() {
-  // original part of default app code, uncomment this if also uncommenting other code
-  //const [count, setCount] = useState(0)
-
   return (
     <>
-    {/* Commented out most of default code, otherwise it shares a page with the component loaded using react router
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR <br/>
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    Temporary Link to Login page, may want to make it the default page later (path='/')
-    We can add other links to the pages you are working on here too*/}
     
     {/* AuthProvider wraps everything to ensure that authentication works properly 
         Be sure to place all components but the Login component within a ProtectedRoute Component*/}
@@ -58,10 +32,8 @@ function App() {
         <Route path='/flicks-leaderboard' element={<ProtectedRoute> <FlicksLeaderboard /> </ProtectedRoute>} />
         <Route path='/reaction-leaderboard' element={<ProtectedRoute> <ReactionLeaderboard /> </ProtectedRoute>} />
         <Route path='/tracing-leaderboard' element={<ProtectedRoute> <TracingLeaderboard /> </ProtectedRoute>} />
+        <Route path='*' element={<Navigate to="/login" replace></Navigate>} />
       </Routes>
-	
-      <p><Link to='/login'>Test link to login page</Link></p>
-	    <p><Link to='/mode'>Test link to modes page</Link></p>
     </AuthProvider>
 	  
     </>

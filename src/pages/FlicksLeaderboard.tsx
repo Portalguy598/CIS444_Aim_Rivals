8db190814482838bd02243763dbc8ecefce85e86
Portalguy598 <ryandmilner@gmail.com>
2026-04-28 15:50:43 -0700

    Added buttons to access mode page from leaderboard pages

--- src/pages/FlicksLeaderboard.tsx
+++ src/pages/FlicksLeaderboard.tsx
@@ -1,10 +1,17 @@
 import LeaderboardWindow from '../LeaderboardWindow';
 import './Leaderboard.css';
+import { useNavigate } from 'react-router-dom';
 
 function FlicksLeaderboard(){
+    const navigate = useNavigate();
+
+    function returnToModeHandler(){
+        navigate('/mode');
+    }
     return(
         <>
             <h1>Flicks Leaderboard</h1>
+            <button onClick={returnToModeHandler}>Back to mode select</button>
             <LeaderboardWindow mode="flick"></LeaderboardWindow>
         </>
     );
